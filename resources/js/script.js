79e01d212c83437f48fa0cafc3f76de394372d42
TANUMON ROY <roytanumon@gmail.com>
2020-07-10 22:28:01 +0530

    indentation update

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,12 @@
 var waypoint = new Waypoint({
-    element: document.querySelector("nav"),
-    handler: (direction) => {
-        if (direction === "down") {
-            var rect = document.querySelector(".main-nav").getBoundingClientRect();
-            //console.log(rect.top);
-            document.querySelector(".main-nav").classList.add("sticky");
-        } else {
-            document.querySelector(".main-nav").classList.remove("sticky");
-        }
-    },
+   element: document.querySelector("nav"),
+   handler: (direction) => {
+      if (direction === "down") {
+         var rect = document.querySelector(".main-nav").getBoundingClientRect();
+         //console.log(rect.top);
+         document.querySelector(".main-nav").classList.add("sticky");
+      } else {
+         document.querySelector(".main-nav").classList.remove("sticky");
+      }
+   },
 });
