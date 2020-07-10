b1317908ef35d2df7c41c83318acb30d19244da8
TANUMON ROY <roytanumon@gmail.com>
2020-07-10 19:55:02 +0530

    sticky nav update

---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+var waypoint = new Waypoint({
+    element: document.querySelector("nav"),
+    handler: (direction) => {
+        if (direction === "down") {
+            var rect = document.querySelector(".main-nav").getBoundingClientRect();
+            //console.log(rect.top);
+            document.querySelector(".main-nav").classList.add("sticky");
+        } else {
+            document.querySelector(".main-nav").classList.remove("sticky");
+        }
+    },
+    offset: "20%",
+});
